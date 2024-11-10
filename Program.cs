718e9a486278f2696eb325408d7f286bb8bc1994
alexvilleda-sion <100446107+alexvilleda-sion@users.noreply.github.com>
2024-11-10 15:19:20 -0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using Ejerciciopolimorfismo2024;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        List<Figura> figuras = new List<Figura>
+        {
+            new Circulo(5),
+            new Rectangulo(4, 6),
+            new Triangulo(3, 7)
+        };
+
+        foreach (Figura figura in figuras)
+        {
+            Console.WriteLine($"El área de la figura es: {figura.CalcularArea()}");
+        }
+    }
+}
---
+++ Rectangulo.cs
@@ -0,0 +1,16 @@
+﻿public class Rectangulo : Figura
+{
+    public double Largo { get; set; }
+    public double Ancho { get; set; }
+
+    public Rectangulo(double largo, double ancho)
+    {
+        Largo = largo;
+        Ancho = ancho;
+    }
+
+    public override double CalcularArea()
+    {
+        return Largo * Ancho;
+    }
+}
